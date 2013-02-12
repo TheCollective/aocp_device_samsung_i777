6ece6cf84836c321266908d93dc9ffa2d69bfaa9
r <r@r-GA-MA78GM-US2H.(none)>
2013-02-12 02:18:07 -0600

    Update dependencies

--- aocp.mk
+++ aocp.mk
@@ -15,7 +15,7 @@
 #
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/aocp/configs/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := SGH-I777
@@ -25,16 +25,24 @@ TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aocp/configs/common_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i777/full_i777.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i777
-PRODUCT_NAME := cm_i777
+PRODUCT_NAME := aocp_i777
 PRODUCT_BRAND := Samsung
+
 PRODUCT_MODEL := SGH-I777
 
 # Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I777 TARGET_DEVICE=SGH-I777 BUILD_FINGERPRINT=samsung/SGH-I777/SGH-I777:4.0.3/IML74K/XXLPQ:user/release-keys PRIVATE_BUILD_DESC="SGH-I777-user 4.0.3 IML74K XXLPQ release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I777 TARGET_DEVICE=SGH-I777 BUILD_FINGERPRINT=samsung/SGH-I777/SGH-I777:4.2.1/JOP40D:user/release-keys PRIVATE_BUILD_DESC="SGH-I777-user 4.2.1 JOP40D release-keys"
+# Release name and versioning
+PRODUCT_RELEASE_NAME := aocp_i777
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+-include v vendor/aocp/products/common_versions.mk
+#bootanimation
+PRODUCT_COPY_FILES += \
+        vendor/aocp/prebuilt/bootanimation/bootanimation_480_800.zip:system/media/bootanimation.zip
